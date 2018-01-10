269d1ffe05be10effa24563cc9b3ff8ad354003d
Fritz <fre.healy@gmail.com>
2018-01-10 16:25:45 -0500

    adding staff template

---
+++ single-staff.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying staff single posts.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package ACStarter
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php
+		while ( have_posts() ) : the_post();
+
+			get_template_part( 'template-parts/content', "staff-single" );
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
---
+++ template-parts/content-staff-single.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template part for displaying page content in single-staff.php.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package ACStarter
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class("template-staff-single"); ?>>
+</article><!-- #post-## -->
