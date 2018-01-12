7b8ac0d2d5fdd85d7e5665713ca3b3c004c52bc5
Fritz <fre.healy@gmail.com>
2018-01-12 13:08:54 -0500

    adding testimonials

--- inc/post-types.php
+++ inc/post-types.php
@@ -38,6 +38,37 @@ function js_custom_init()
   ); 
   register_post_type('staff',$args); // name used in query
   
+  $labels = array(
+	'name' => _x('Testimonials', 'post type general name'),
+    'singular_name' => _x('Testimonial', 'post type singular name'),
+    'add_new' => _x('Add New', 'Testimonial'),
+    'add_new_item' => __('Add New Testimonial'),
+    'edit_item' => __('Edit Testimonial'),
+    'new_item' => __('New Testimonial'),
+    'view_item' => __('View Testimonial'),
+    'search_items' => __('Search Staff'),
+    'not_found' =>  __('No Testimonials found'),
+    'not_found_in_trash' => __('No Testimonials found in Trash'), 
+    'parent_item_colon' => '',
+    'menu_name' => 'Testimonials'
+  );
+  $args = array(
+	'labels' => $labels,
+    'public' => true,
+    'publicly_queryable' => true,
+    'show_ui' => true, 
+    'show_in_menu' => true, 
+    'query_var' => true,
+    'rewrite' => true,
+    'capability_type' => 'post',
+    'has_archive' => false, 
+    'hierarchical' => false, // 'false' acts like posts 'true' acts like pages
+    'menu_position' => 20,
+    'supports' => array('title','editor','custom-fields','thumbnail'),
+	
+  ); 
+  register_post_type('testimonial',$args); // name used in query
+  
   // Add more between here
   
   // and here
