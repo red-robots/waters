9c5c533f80ec38a6201b2f1f7162d903082e1b9b
Fritz <fre.healy@gmail.com>
2018-01-15 10:15:42 -0500

    adding projects cpt

--- inc/post-types.php
+++ inc/post-types.php
@@ -69,6 +69,37 @@ function js_custom_init()
   ); 
   register_post_type('testimonial',$args); // name used in query
   
+  $labels = array(
+	'name' => _x('Projects', 'post type general name'),
+    'singular_name' => _x('Project', 'post type singular name'),
+    'add_new' => _x('Add New', 'Project'),
+    'add_new_item' => __('Add New Project'),
+    'edit_item' => __('Edit Project'),
+    'new_item' => __('New Project'),
+    'view_item' => __('View Project'),
+    'search_items' => __('Search Projects'),
+    'not_found' =>  __('No Projects found'),
+    'not_found_in_trash' => __('No Projects found in Trash'), 
+    'parent_item_colon' => '',
+    'menu_name' => 'Projects'
+  );
+  $args = array(
+	'labels' => $labels,
+    'public' => true,
+    'publicly_queryable' => true,
+    'show_ui' => true, 
+    'show_in_menu' => true, 
+    'query_var' => true,
+    'rewrite' => true,
+    'capability_type' => 'post',
+    'has_archive' => false, 
+    'hierarchical' => false, // 'false' acts like posts 'true' acts like pages
+    'menu_position' => 20,
+    'supports' => array('title','editor','custom-fields','thumbnail'),
+	
+  ); 
+  register_post_type('project',$args); // name used in query
+  
   // Add more between here
   
   // and here
