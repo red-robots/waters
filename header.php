5f0c7436254f24abbc733c69bc65d60ded6c7d81
Fritz <fre.healy@gmail.com>
2018-02-01 11:36:07 -0500

    adding login logo

--- header.php
+++ header.php
@@ -30,11 +30,11 @@ if($ga) echo $ga;?>
 			<div class="wrapper cap">		
 				<?php if(is_home()): ?>
 					<h1 class="logo">
-						<a href="<?php bloginfo('url'); ?>"><img src="<?php echo get_template_directory_uri()."/images/logo.jpg";?>" alt="<?php bloginfo('name'); ?>"></a>
+						<a href="<?php bloginfo('url'); ?>"><img src="<?php echo get_template_directory_uri()."/images/logo.png";?>" alt="<?php bloginfo('name'); ?>"></a>
 					</h1>
 				<?php else: ?>
 					<div class="logo">
-						<a href="<?php bloginfo('url'); ?>"><img src="<?php echo get_template_directory_uri()."/images/logo.jpg";?>" alt="<?php bloginfo('name'); ?>"></a>
+						<a href="<?php bloginfo('url'); ?>"><img src="<?php echo get_template_directory_uri()."/images/logo.png";?>" alt="<?php bloginfo('name'); ?>"></a>
 					</div>
 				<?php endif; ?>
 				<nav id="site-navigation" class="main-navigation" role="navigation">
--- inc/theme.php
+++ inc/theme.php
@@ -14,9 +14,9 @@ function my_login_logo() { ?>
 <style type="text/css">
   body.login div#login h1 a {
   	background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png);
-  	background-size: 327px 67px;
+  	background-size: 327px 120px;
   	width: 327px;
-  	height: 67px;
+  	height: 120px;
   }
 </style>
 <?php }
