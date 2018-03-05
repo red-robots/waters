38dd68539aae259a435443229fa85c0e318239ec
Fritz <fre.healy@gmail.com>
2018-03-05 09:58:08 -0500

    adding single testimonial pickers

--- sidebar-history.php
+++ sidebar-history.php
@@ -27,23 +27,27 @@
 			<a class="button" href="<?php echo $callout_link;?>"><?php echo $callout_link_text;?></a>
 		</div><!--.wrapper-->
 	<?php endif;
-	$args = array(
-		'post_type'=>'testimonial',
-		'post_per_page'=>1,
-		'order'=>'ASC',
-		'orderby'=>'rand'
-	);
-	$query = new WP_Query($args);
-	if($query->have_posts()): $query->the_post();
-		if($testimonials_title):?>
-			<header>
-				<h2><?php echo $testimonials_title;?></h2>
-			</header>
-		<?php endif;?>
-		<div class="testimonial copy">
-			<?php the_content();?>
-		</div><!--.testimonial.copy-->
-	<?php wp_reset_postdata();
+	$testimonial = get_field("testimonial_picker");
+	if($testimonial):
+		$args = array(
+			'post_type'=>'testimonial',
+			'post_per_page'=>1,
+			'order'=>'ASC',
+			'post__in'=>array($testimonial),
+			'orderby'=>'rand'
+		);
+		$query = new WP_Query($args);
+		if($query->have_posts()): $query->the_post();
+			if($testimonials_title):?>
+				<header>
+					<h2><?php echo $testimonials_title;?></h2>
+				</header>
+			<?php endif;?>
+			<div class="testimonial copy">
+				<?php the_content();?>
+			</div><!--.testimonial.copy-->
+		<?php wp_reset_postdata();
+		endif;
 	endif;
 	$affiliations_title = get_field("affiliations_title","option");
 	if($affiliations_title):?>
--- sidebar-projects.php
+++ sidebar-projects.php
@@ -7,24 +7,27 @@
  */ ?>
 
 <aside class="col-2 sidebar">
-	<?php $callout_title = get_field("callout_title","option");
-	$callout_copy = get_field("callout_copy","option");
-	$callout_link = get_field("callout_link","option");
-	$callout_link_text = get_field("callout_link_text","option");
-	$archives_title = get_field("archives_title","option");
-	if($callout_title):?>
-		<header>
-			<h2><?php echo $callout_title;?></h2>
-		</header>
-	<?php endif;
-	if($callout_copy):?>
-		<div class="copy">
-			<?php echo $callout_copy;?>
-		</div><!--.copy-->
-	<?php endif;?>
-	<?php if($callout_link&&$callout_link_text):?>
-		<div class="wrapper">
-			<a class="button" href="<?php echo $callout_link;?>"><?php echo $callout_link_text;?></a>
-		</div><!--.wrapper-->
-	<?php endif;?>
+	<?php $testimonials_title = get_field("testimonials_title","option");
+	$testimonial = get_field("testimonial_picker");
+	if($testimonial):
+		$args = array(
+			'post_type'=>'testimonial',
+			'post_per_page'=>1,
+			'order'=>'ASC',
+			'post__in'=>array($testimonial),
+			'orderby'=>'rand'
+		);
+		$query = new WP_Query($args);
+		if($query->have_posts()): $query->the_post();
+			if($testimonials_title):?>
+				<header>
+					<h2><?php echo $testimonials_title;?></h2>
+				</header>
+			<?php endif;?>
+			<div class="testimonial copy">
+				<?php the_content();?>
+			</div><!--.testimonial.copy-->
+		<?php wp_reset_postdata();
+		endif;
+	endif;?>
 </aside><!--.col-2-->
